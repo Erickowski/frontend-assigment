8cb92d091a3536f668dabe296e1c73206d33e388
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-23 17:16:52 -0500

    Layout finalizado

---
+++ components/Layout.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+import Head from "next/head";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          :root {
+            --blue: #1b80c7;
+            --lightblue: #c8dff1;
+            --green: #65b32e;
+            --white: #edebeb;
+            --black: #010101;
+          }
+          html {
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+        `}
+      />
+      <Head>
+        <html lang="es" />
+        <title>DocRed Mockup</title>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
